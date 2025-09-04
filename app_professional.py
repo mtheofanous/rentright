499b5f65c90679052cedeccd1a080939cb30bbf1
Martheo <theofanousmario@gmail.com>
2025-09-04 19:13:04 +0200

    s

--- app_professional.py
+++ app_professional.py
@@ -3602,7 +3602,7 @@ def tenant_dashboard():
                                     <div class="prop-card">
                                     <div class="prop-title">• {addr}</div>
                                     <div class="prop-sub">{chips_html}</div>
-                                    <div class="prop-foot">{tr('Updated')}: {upd}{link_html}</div>
+                                    <div class="prop-foot">{tr('Updated')}: {format_dt(upd)}{link_html}</div>
                                     </div>
                                     """,
                                     unsafe_allow_html=True
