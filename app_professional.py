248456f8f3b00d9530858f955ab96e2b825f799b
Martheo <theofanousmario@gmail.com>
2025-09-06 15:58:36 +0200

    s

--- app_professional.py
+++ app_professional.py
@@ -6135,7 +6135,7 @@ def landlord_dashboard():
                             t_city, t_district,
                             t_smin, t_smax, t_rmin, t_rmax, t_fmin, t_fmax, t_pmin, t_pmax
                         ) = r
-                        
+                    with st.container(border=True):    
                         tenant_profile(tenant_id, landlord_id, inbound_request=False)
 
 
